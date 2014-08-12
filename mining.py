16bb6acba056a4bd1d18aa56947999dc870d5206
psig <psigur@gmail.com>
2014-08-11 22:36:34 -0400

    data and main script
    cleaned up data and added entropy and info gain functions to main script

--- mining.py
+++ mining.py
@@ -1,28 +1,65 @@
 """"
-  Respondent data structure:
-  
-  respondents = { for each id in dataset: {"Age": ##, "Gender": #,
-                       "Income": #, "Education": #, "Arrested": ##,
-                       "Jailed": ##, "Tobacco": #,  "Alcohol": ##,
-                       "IllegalDrugs": #, "Depression": ##, 
-                       "AlcoholOrDrugTreatment": ##, 
-                       "MentalHealthTreatment": ##}}
-"""
+  mining.py: Decision Tree Classifier
 
+"""
 import time
+import csv
+import math
+
+#for testing script run time
 start_time = time.perf_counter()
 
 #open CSV containing data
-import csv
-with open('data-subset.csv') as f:
-  reader = csv.reader(f)
-  #load data into dictionary
-  respondents = {rows[0]: {"Age": rows[1], "Gender": rows[2],
-                       "Income": rows[3], "Education": rows[4],
-                       "Arrested": rows[5], "Jailed": rows[6],
-                       "Tobacco": rows[7],  "Alcohol": rows[8],
-                       "IllegalDrugs": rows[9], "Depression": rows[10], 
-                       "AlcoholOrDrugTreatment": rows[11], 
-                       "MentalHealthTreatment": rows[12]} for rows in reader}
+with open('clean-data-subset.csv') as file:
+  reader = csv.reader(file)
+  #load data into list of tuples
+  columns = ('Age','Gender','Income','Education','Tobacco',
+             'Alcohol','IllegalDrugs','Depression',
+             'AlcoholOrDrugTreatment','MentalHealthTreatment',
+             'ArrestedOrJailed')
+  data = [tuple(row) for row in reader]
 
+#split data into training and testing lists
+
+
+  #check for base cases
+
+  #for each attribute, find the info gain for splitting on that attribute
+
+  #create a decision node that splits on the attribute with highest gain
+
+  #apply steps to each sublist
+
+def entropy(data):
+  """Returns entropy of dataset based on class label in final position """
+  totalRecords = len(data)
+  classRecords = sum(1 for element in data if element[len(element)-1] == '1')
+  nonClassRecords = totalRecords - classRecords
+  entropy = ( - (classRecords/totalRecords) 
+              * math.log(classRecords/totalRecords)
+              - (nonClassRecords/totalRecords) 
+              * math.log(nonClassRecords/totalRecords))
+  return(entropy)
+
+def information_gain(data, element):
+  """Returns information gain for splitting data on specified element """
+  currentEntropy = entropy(data)
+
+  #split data into sublists
+  sublists = {}
+  for row in data:
+    if row[element] not in sublists:
+      sublists[row[element]] = [row]
+    else:
+      sublists[row[element]].append(row)
+  
+  #get entropy for each sublist
+  sublistEntropy = 0
+  for key in sublists:
+    sublistEntropy += entropy(sublists[key]) * (len(sublists[key]) / len(data))
+  return(currentEntropy - sublistEntropy)
+
+print("Initial entropy:",entropy(data))
+for x in range(0, 10):
+  print("Info gain for splitting on ",columns[x],": ",information_gain(data, x))
 print(time.perf_counter() - start_time)
